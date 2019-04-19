cdc25ca7c8268093ae30a5319c3aab254a28d2bc
fengn <fengnmail@foxmail.com>
2019-04-20 00:29:58 +0800

    connect to mongo api.js modified

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,15 +3,54 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const article = require('../models/article');
 
-const db = "mongodb+srv://fengnan:asd%2E1234@cluster0-pddmv.mongodb.net/test?retryWrites=true";
+//const db = "mongodb+srv://fengnan:asd%2E1234@cluster0-pddmv.mongodb.net/test?retryWrites=true";
+// mongoose.connect('mongodb+srv://fengnan:asd%2E1234@cluster0-pddmv.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   // we're connected!
+//   console.log('Success connecting!');
+// });
+var MongoClient = require('mongodb').MongoClient;
+
+var url = "mongodb://localhost:27017/learning_mongo";
+
+// MongoClient.connect(url, function(err, db) {
+// 	console.log("Connected successfully to server");
+
+// 	db.close();
+// })
+// var findDocuments = function(db, callback) {
+// 	var collection = db.collection('tours');
+
+// 	collection.find().toArray(function(err, docs) {
+// 		console.log(docs);
+// 		callback;
+// 	})
+// }
+
+const client = new MongoClient(url, { useNewUrlParser: true });
 
 
-mongoose.Promise = global.Promise;
-mongoose.connect(db, function(err){
-	if(err) {
-		console.log('Error connecting');
-	}
+client.connect(err => {
+  const collection = client.db("learning_mongo").collection("tours");
+  // perform actions on the collection object
+  collection.find({"tourPrice" : 1100}).toArray(function(err, docs) {
+		console.log(docs);
+		//callback;
+	})
+  console.log("Connected successfully to server");
+
+	//db.close();
+	client.close();
 });
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect(db, function(err){
+// 	if(err) {
+// 		console.log('Error connecting1');
+// 	}
+// });
 
 router.get('/all', function(req, res) {
 
